22ab535a3496caecdf1a4974938da9bab62b81b0
Hamdy Tawfeek <hamdymostawfeek@gmail.com>
2023-12-31 17:35:58 +0100

    Add endpoint to query image frames

--- app/main.py
+++ app/main.py
@@ -1,7 +1,26 @@
 from fastapi import FastAPI
+from app.db import engine
+from app.frames import adjust_and_store_frames
+from sqlalchemy import text
 
 
-app = FastAPI()
+def start_application():
+    app = FastAPI()
+    adjust_and_store_frames()
+    return app
+
+app = start_application()
+
+
+@app.get("/frames/")
+def read_item(depth_min: float = 9000.1, depth_max: float = 9546.0):
+    conn = engine.connect()
+    output = conn.execute(
+        text(f"SELECT * FROM data WHERE depth BETWEEN {depth_min} AND {depth_max}")
+    )
+    results = output.mappings().fetchall()
+    conn.close()
+    return {"image_frames": results}
 
 @app.get("/")
 def home():
